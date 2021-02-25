ef8336ad4503082b817df410415d90b0dffde8f7
Derek Cruz <Derekcruz94@gmail.com>
2021-02-25 14:57:21 -0800

    Completed convertHoursToMinutes.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(number) {
+  return number;
+}
+
+console.log(convertHoursToMinutes(60));
